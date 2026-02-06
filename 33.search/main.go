be7701a4aefcc18be7d6aa4aed0e50622a5c227a
v_lidongze <v_lidongze@baidu.com>
2026-02-06 00:47:28 -0800

    feat: LeetCode 旋转数组查找的一次性二分搜索方式

--- 33.search/main.go
+++ 33.search/main.go
@@ -43,11 +43,35 @@ func search(nums []int, target int) int {
 	return lowerBound(nums, i, len(nums)-1, target) // [i, n-1)
 }
 
-func search2(nums []int, target int) {
+// [0,n-1)
+// 3 3 4 5 1 2 3 3 3 3 3 3 3
+func search2(nums []int, target int) int {
+	flag := nums[len(nums)-1]
+	left, right := 0, len(nums)-1
+	middle := 0
 
+	for left < right {
+		middle = left + (right-left)/2
+		if nums[middle] <= flag && target > flag {
+			right = middle
+		} else if nums[middle] > flag && target <= flag {
+			left = middle + 1
+		} else if nums[middle] > target {
+			right = middle
+		} else if nums[middle] < target {
+			left = middle + 1
+		} else if nums[middle] == target {
+			right = middle
+		}
+	}
+	if nums[left] != target {
+		return -1
+	}
+	return left
 }
 
 func main() {
+	fmt.Println(search2([]int{3, 3, 4, 5, 1, 2, 3, 3, 3}, 3))
 	// 153,确定最小值位置划分区间，然后对区间进行二分查找
 	src := []int{4, 5, 6, 7, 0, 1, 2}
 	fmt.Println(search(src, 1))
