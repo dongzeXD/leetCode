653e1787581642803e5440c21d7cda348e7aca94
v_lidongze <v_lidongze@baidu.com>
2026-03-16 19:30:54 +0800

    feat: 53 个人推导过程及复习

--- 53.max_sub_array/main.go
+++ 53.max_sub_array/main.go
@@ -17,6 +17,26 @@ func maxSubArray(nums []int) int {
 	return maxSum
 }
 
+// -1 -2 4 -5 2 9 -2
+// m:-1 c:-1 max:-1
+// m:-3 c:-3 max: -1
+// m:-3 c:1  max: 4
+// m:-4 c:-4 max:4
+// m:-4 c:-2 max:4
+//
+//	m:-4 c: 7 max: 11
+func maxSubArray2(nums []int) int {
+	minPre := 0
+	maxSum := math.MinInt
+	curSum := 0
+	for _, num := range nums {
+		curSum += num
+		maxSum = max(maxSum, curSum-minPre)
+		minPre = min(minPre, curSum)
+	}
+	return maxSum
+}
+
 func main() {
 	r := maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})
 	fmt.Println(r)
